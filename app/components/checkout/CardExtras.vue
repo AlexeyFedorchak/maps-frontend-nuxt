990ebe33e1799bce7bd6856f447dfeae58442799
Bogdan Rosynskyi <bogdan@darly.solutions>
2025-09-08 16:23:53 +0300

    add some part of markup for checkout page

---
+++ app/components/checkout/CardExtras.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Recommendation {
+  id: number;
+  title: string;
+  subTitle: string;
+  price: string;
+}
+
+const props = defineProps<{
+  recommendations: Array<Recommendation>;
+}>();
+const emit = defineEmits<{
+  (e: 'select', item: Recommendation): void;
+}>();
+</script>
+
+<template>
+  <div class="md:mt-6">
+    <div class="inline-block md:bg-[#f7f7f7]">
+      <h3 class="uppercase font-semibold black p-2 text-[14px]">Recommended Extras</h3>
+    </div>
+    <div class="p-6 space-y-3 md:bg-[#f7f7f7]">
+      <div v-for="item of props.recommendations"
+           :key="item.id"
+           class="flex justify-between items-center">
+        <div>
+          <p class="font-medium">{{item.title}}</p>
+          <p class="text-sm text-gray-500">{{item.subTitle}}</p>
+        </div>
+        <button class="text-blue-600 font-medium text-sm" @click="emit('select', item)">ADD</button>
+        <p class="text-sm">{{item.price}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/checkout/index.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const basket = [
+  {
+    id: 1,
+    title: "STAR MAP",
+    location: "London UK",
+    date: "01 Jan 2025",
+    printSize: "Print Size: 40cm x 50 cm",
+    price: "£31.99",
+    recommendations: [
+      {
+        id: 1,
+        title: "GIFT WRAP",
+        subTitle: "Ribbon gift wrap",
+        price: "£2.49",
+      },
+      {
+        id: 2,
+        title: "WALL STRIPS",
+        subTitle: "Easy hassle-free fixture",
+        price: "£2.99",
+      },
+      {
+        id: 3,
+        title: "GREETINGS CARD",
+        subTitle: "Add personal message",
+        price: "£0.99",
+      },
+    ],
+  },
+];
+
+function addExtra(item: any) {
+  console.log('addExtra', item);
+}
+
+</script>
+
+<template>
+  <UiLayoutHeader/>
+  <div class="max-w-5xl mx-auto px-4 py-8">
+    <h1 class="hidden md:block text-center text-xl font-bold tracking-wide mb-6">YOUR BASKET</h1>
+    <CheckoutCard
+        v-for="item of basket"
+        :key="item.id"
+        :title="item.title"
+        :location="item.location"
+        :date="item.date"
+        :print-size="item.printSize"
+        :price="item.price"
+    >
+      <template #extras>
+        <CheckoutCardExtras :recommendations="item.recommendations" @select="addExtra($event)"/>
+      </template>
+    </CheckoutCard>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
